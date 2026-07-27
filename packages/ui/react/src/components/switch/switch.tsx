8c373b03abf6c019b680ba9d1222011a4f008c69
elowanaud <elowan.audouin@icloud.com>
2026-07-27 20:59:58 +0200

    fix(ui): add extra touch zone to switch component

--- packages/ui/react/src/components/switch/switch.tsx
+++ packages/ui/react/src/components/switch/switch.tsx
@@ -28,13 +28,14 @@ export function SwitchRoot(props: SwitchRootProps) {
 	return (
 		<SwitchHeadless.Root
 			className={cn(
-				"group flex h-6 w-9 items-center rounded-full border-2 outline-none ring-primary-8 transition",
+				"group relative flex h-6 w-9 cursor-pointer items-center rounded-full border-2 outline-none ring-primary-8 transition",
 				"data-unchecked:border-neutral-9 data-unchecked:bg-neutral-9",
 				"data-unchecked:hover:not-disabled:border-neutral-10 data-unchecked:hover:not-disabled:bg-neutral-10",
 				"data-checked:border-primary-9 data-checked:bg-primary-9",
 				"data-checked:hover:not-disabled:border-primary-10 data-checked:hover:not-disabled:bg-primary-10",
 				"focus-visible:ring-3",
 				"data-disabled:cursor-not-allowed data-disabled:opacity-50",
+				"before:absolute before:-inset-2",
 				className,
 			)}
 			{...rest}
