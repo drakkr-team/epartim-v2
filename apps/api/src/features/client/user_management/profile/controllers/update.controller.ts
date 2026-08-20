6eea7e85b8c22a2ae03f0d2eb3d5415c69af1790
elowanaud <elowan.audouin@icloud.com>
2026-08-20 17:16:03 +0200

    fix: typecheck

--- apps/api/src/features/client/user_management/profile/controllers/update.controller.ts
+++ apps/api/src/features/client/user_management/profile/controllers/update.controller.ts
@@ -3,6 +3,7 @@ import { HttpContext } from "@adonisjs/core/http";
 import vine from "@vinejs/vine";
 
 import UpdateProfilePolicy from "#features/client/user_management/profile/policies/update.policy";
+import User from "#models/user";
 import UserPresenter from "#presenters/user.presenter";
 import { UpdateUserSchema } from "#validators/user.validator";
 
@@ -13,7 +14,7 @@ export default class UpdateProfileController {
 	async handle({ request, auth, bouncer }: HttpContext) {
 		await bouncer.with(UpdateProfilePolicy).authorize("handle");
 
-		const user = auth.user!;
+		const user = auth.user as User;
 
 		const payload = await request.validateUsing(UpdateProfileController.payloadSchema);
 
