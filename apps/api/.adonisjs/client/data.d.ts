69be08ec0f84bc20e55519de9d894f7b2d158c2e
MatisseDL <mat.dl17000@gmail.com>
2026-08-21 10:46:37 +0200

    feat: add PaymentDetail type and presenter for payment details

--- apps/api/.adonisjs/client/data.d.ts
+++ apps/api/.adonisjs/client/data.d.ts
@@ -17,3 +17,5 @@ export type Network = Awaited<ReturnType<typeof import("#presenters/network.pres
 export type Pagination = Awaited<ReturnType<typeof import("#presenters/pagination.presenter").default.prototype.toJSON>>;
 
 export type User = Awaited<ReturnType<typeof import("#presenters/user.presenter").default.prototype.toJSON>>;
+
+export type PaymentDetail = Awaited<ReturnType<typeof import("#presenters/payment_detail.presenter").default.prototype.toJSON>>;
