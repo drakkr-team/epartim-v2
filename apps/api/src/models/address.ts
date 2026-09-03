3754962ff06f8250defa5b7f75a33f2fa5b3d832
MatisseDL <mat.dl17000@gmail.com>
2026-09-03 17:05:35 +0200

    refactor(address): remove unused hasmany in address

--- apps/api/src/models/address.ts
+++ apps/api/src/models/address.ts
@@ -1,10 +1,3 @@
-import { hasMany } from "@adonisjs/lucid/orm";
-import type { HasMany } from "@adonisjs/lucid/types/relations";
+import {AddressSchema} from "#database/schema";
 
-import { AddressSchema } from "#database/schema";
-import Network from "#models/network";
-
-export default class Address extends AddressSchema {
-	@hasMany(() => Network)
-	declare networks: HasMany<typeof Network>;
-}
+export default class Address extends AddressSchema {}
