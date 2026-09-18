6d43e6fb433a1a7faf4f235146a68985930b7c46
elowanaud <elowan.audouin@icloud.com>
2026-09-18 17:34:25 +0200

    fix(api): add activate policy

--- apps/api/src/features/admin/account_management/onboarding/controllers/activate.controller.ts
+++ apps/api/src/features/admin/account_management/onboarding/controllers/activate.controller.ts
@@ -2,6 +2,7 @@ import { inject } from "@adonisjs/core";
 import { HttpContext } from "@adonisjs/core/http";
 import vine from "@vinejs/vine";
 
+import ActivatePolicy from "#features/admin/account_management/onboarding/policies/activate.policy";
 import AdminOnboardingService from "#features/admin/account_management/onboarding/services/onboarding.service";
 import { UserPasswordValidator } from "#validators/user.validator";
 
@@ -9,7 +10,9 @@ import { UserPasswordValidator } from "#validators/user.validator";
 export default class ActivateAdminOnboardingController {
 	constructor(protected onboardingService: AdminOnboardingService) {}
 
-	async handle({ request, response }: HttpContext) {
+	async handle({ request, response, bouncer }: HttpContext) {
+		await bouncer.with(ActivatePolicy).authorize("handle");
+
 		const { token, newPassword } = await request.validateUsing(
 			ActivateAdminOnboardingController.payloadSchema,
 		);
