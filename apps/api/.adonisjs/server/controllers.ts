2f21571319519f5f23fa59d6c48f425d910a8197
MatisseDL <mat.dl17000@gmail.com>
2026-09-18 17:19:05 +0200

    refactor(kyc): reorganize user controllers and restore beneficial owner type

--- apps/api/.adonisjs/server/controllers.ts
+++ apps/api/.adonisjs/server/controllers.ts
@@ -92,9 +92,6 @@ export const controllers = {
         },
         update: {
           AddressAndBankDetails: () => import('#src/features/client/subscriptions/controllers/update/address_and_bank_details.controller'),
-          KycOwnerCreate: () => import('#src/features/client/subscriptions/controllers/update/kyc_owner_create.controller'),
-          KycOwnerDelete: () => import('#src/features/client/subscriptions/controllers/update/kyc_owner_delete.controller'),
-          KycOwnerUpdate: () => import('#src/features/client/subscriptions/controllers/update/kyc_owner_update.controller'),
           KycProfile: () => import('#src/features/client/subscriptions/controllers/update/kyc_profile.controller'),
           LegalIdentification: () => import('#src/features/client/subscriptions/controllers/update/legal_identification.controller'),
           representatives: {
@@ -103,6 +100,9 @@ export const controllers = {
             LegalAgent: () => import('#src/features/client/subscriptions/controllers/update/representatives/legal_agent.controller'),
             Signer: () => import('#src/features/client/subscriptions/controllers/update/representatives/signer.controller'),
           },
+          KycOwnerCreate: () => import('#src/features/client/subscriptions/controllers/update/kyc_owner_create.controller'),
+          KycOwnerDelete: () => import('#src/features/client/subscriptions/controllers/update/kyc_owner_delete.controller'),
+          KycOwnerUpdate: () => import('#src/features/client/subscriptions/controllers/update/kyc_owner_update.controller'),
         },
         View: () => import('#src/features/client/subscriptions/controllers/view.controller'),
       },
