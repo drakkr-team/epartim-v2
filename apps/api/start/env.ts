91208ed7cfcfe2f26f88d9f2e6457f0501dcf28f
elowanaud <elowan.audouin@icloud.com>
2026-09-25 13:28:02 +0200

    fix: test

--- apps/api/start/env.ts
+++ apps/api/start/env.ts
@@ -63,12 +63,9 @@ export default await Env.create(new URL("../", import.meta.url), {
 	S3_BUCKET: Env.schema.string.optionalWhen(process.env.DRIVE_DRIVER !== "s3"),
 
 	// DocuSign Config
-	DOCUSIGN_BASE_PATH: Env.schema.string.optionalWhen(process.env.NODE_ENV !== "test", {
-		format: "url",
-		tld: true,
-	}),
-	DOCUSIGN_CLIENT_ID: Env.schema.string.optionalWhen(process.env.NODE_ENV !== "test"),
-	DOCUSIGN_USER_ID: Env.schema.string.optionalWhen(process.env.NODE_ENV !== "test"),
-	DOCUSIGN_ACCOUNT_ID: Env.schema.string.optionalWhen(process.env.NODE_ENV !== "test"),
-	DOCUSIGN_RSA_PRIVATE_KEY: Env.schema.string.optionalWhen(process.env.NODE_ENV !== "test"),
+	DOCUSIGN_BASE_PATH: Env.schema.string({ format: "url", tld: true }),
+	DOCUSIGN_CLIENT_ID: Env.schema.string(),
+	DOCUSIGN_USER_ID: Env.schema.string(),
+	DOCUSIGN_ACCOUNT_ID: Env.schema.string(),
+	DOCUSIGN_RSA_PRIVATE_KEY: Env.schema.string(),
 });
